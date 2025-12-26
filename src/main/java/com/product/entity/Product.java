3366a69cc899cc971eb5a82db0f1fe1df143419e
NitUser <gond.angad@gmail.com>
2025-12-26 21:22:31 +0530

    Added code for product

---
+++ src/main/java/com/product/entity/Product.java
@@ -0,0 +1,39 @@
+package com.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    String description;
+
+    BigDecimal price;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+}
+
